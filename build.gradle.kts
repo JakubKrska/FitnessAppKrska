a2c09a4a6ceee4f538d8af63a793de39182782e9
Bubak <st67056@upce.cz>
2025-07-20 04:54:04 +0200

    První commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val h2_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
+val exposed_version = "0.45.0"
 
 plugins {
     kotlin("jvm") version "2.1.20"
@@ -38,12 +39,24 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.2")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.43.0")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.45.0") // nebo aktuální verzi
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0") // pokud používáš Kotlinx-datetime
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    tasks.withType<Jar> {
+        manifest {
+            attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+        }
+
+
+        from({
+            configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+        })
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
 }
